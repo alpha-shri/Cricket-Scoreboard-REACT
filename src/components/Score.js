253e63c0568eee798cca58ca4804287fea79f3d3
Shrijeet Kumar Panda <shrijeetkumarpanda@gmail.com>
2021-04-01 12:08:09 +0530

    initial commit

---
+++ src/components/Score.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Score() {
+    return (
+        <div className="Score">
+            <h2>Hello Scoreboard</h2>            
+        </div>
+    )
+}
+
+export default Score;
